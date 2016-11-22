d4d7314a86014bb1032be20f9625d8e7f15960f4
DivyaVipin <divyababu1001@gmail.com>
2016-11-22 21:22:20 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,24 @@
+public class Test {
+
+	static{
+
+		print(10);
+
+	}
+
+	
+
+	static void print(int x) {
+
+		System.out.println (x);
+
+		System.exit(0);
+
+	}
+}
+/*
+Program compiled successfully.Error thrown is closest to option b.
+Error shown is Main method not found in class Test
+
+*/
+
